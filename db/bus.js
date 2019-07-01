16a9568a43cb91c9e995aa24d105499e1ced8983
Gtstride <lewisagho47@gmail.com>
2019-07-01 03:39:33 +0100

    Update this files

--- db/bus.js
+++ db/bus.js
@@ -2,10 +2,46 @@ const buses = [
   {
     id: 1,
     number_plate: 'I5I-DL-2308',
-    manufacturer: 'Volvo',
+    manufacturer: 'America',
     model: 'Volvo 9400XL BUS',
-    year: '20-15-1=2013',
+    year: '28-3-2011',
     capacity: 40,
+  },
+
+  {
+    id: 1,
+    number_plate: 'I5I-DL-2308',
+    manufacturer: 'China',
+    model: 'Volvo 9400XL BUS',
+    year: '20-4-200',
+    capacity: 18,
+  },
+
+  {
+    id: 1,
+    number_plate: '08-KY-667',
+    manufacturer: 'Japan',
+    model: 'Volvo B9R Bus',
+    year: '1-1-2012',
+    capacity: 34,
+  },
+
+  {
+    id: 1,
+    number_plate: 'GH-TYU-5867',
+    manufacturer: 'Germany',
+    model: 'Toyota',
+    year: '20-15-2018',
+    capacity: 24,
+  },
+
+  {
+    id: 1,
+    number_plate: 'SAM-PLE',
+    manufacturer: 'Misturbushi',
+    model: 'Misturbush VI987 BUS',
+    year: '2-5-1999',
+    capacity: 16,
   },
 ];
 
