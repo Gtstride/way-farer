388cce6240ce3224578cb91bfcf0788a2d132848
Gtstride <lewisagho47@gmail.com>
2019-07-01 03:26:14 +0100

    Add db for bus

---
+++ db/bus.js
@@ -0,0 +1,12 @@
+const buses = [
+  {
+    id: 1,
+    number_plate: 'I5I-DL-2308',
+    manufacturer: 'Volvo',
+    model: 'Volvo 9400XL BUS',
+    year: '20-15-1=2013',
+    capacity: 40,
+  },
+];
+
+export default buses;
